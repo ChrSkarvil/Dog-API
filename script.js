56ea1faf01610b809a8d6f528312283c255de793
ChrSkarvil <christian.skarvil@live.dk>
2021-11-17 12:04:34 +0100

    Added slideshow that changes picture every 3 seconds

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let timer;
+let deleteImageDelay;
+
 async function start() {
   const response = await fetch("https://dog.ceo/api/breeds/list/all");
   const data = await response.json();
@@ -24,5 +27,44 @@ async function loadByBreed(breed) {
     const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
     const data = await response.json();
     console.log(data);
+    createSlideshow(data.message);
+  }
+}
+
+function createSlideshow(images) {
+  let currentImage = 0;
+  clearInterval(timer);
+  clearTimeout(deleteImageDelay);
+
+  if (images.length > 1) {
+    document.getElementById("slideshow").innerHTML = `
+    <div class="slide" style="background-image: url('${images[0]}');"></div>
+    <div class="slide" style="background-image: url('${images[1]}');"></div>
+    `;
+    currentImage += 2;
+    if (images.length == 2) currentImage = 0;
+    timer = setInterval(nextSlide, 3000);
+  } else {
+    document.getElementById("slideshow").innerHTML = `
+    <div class="slide" style="background-image: url('${images[0]}');"></div>
+    <div class="slide"></div>
+    `;
+  }
+
+  function nextSlide() {
+    document
+      .getElementById("slideshow")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<div class="slide" style="background-image: url('${images[currentImage]}')"></div>`
+      );
+    deleteImageDelay = setTimeout(function () {
+      document.querySelector(".slide").remove();
+    }, 1000);
+    if (currentImage + 1 >= images.length) {
+      currentImage = 0;
+    } else {
+      currentImage++;
+    }
   }
 }
